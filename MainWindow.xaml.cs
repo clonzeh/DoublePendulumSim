f2ee34c13845af6d021737e497d538bb0e85c50d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2014-07-09 11:29:26 +0200

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -37,6 +37,7 @@ namespace DoublePendulumSim
             this.Phi1 = Phi1;
             this.Phi2 = Phi2;
             Create(canvas);
+            Update();
         }
 
         public void Create(Canvas canvas)
@@ -45,42 +46,40 @@ namespace DoublePendulumSim
             myLine2 = new Line { X1 = 0, Y1 = 0, X2 = 0, Y2 = 0, StrokeThickness = 5, Stroke = Brushes.Red };
             myCircle1 = new Ellipse { Width = 2*m1, Height = 2*m1, Fill = Brushes.Black };
             myCircle2 = new Ellipse { Width = 2*m2, Height = 2*m2, Fill = Brushes.Black };
-            Canvas.SetLeft(myCircle1, X0+l1*Math.Sin(Phi1) - m1);
-            Canvas.SetTop(myCircle1, Y0+l1*Math.Cos(Phi1) - m1);
-            Canvas.SetLeft(myCircle2, X0+l1*Math.Sin(Phi1)+l2*Math.Sin(Phi2) - m2);
-            Canvas.SetTop(myCircle2, Y0+l1*Math.Cos(Phi1)+l2*Math.Cos(Phi2) - m2);
             canvas.Children.Add(myLine1);
             canvas.Children.Add(myLine2);
             canvas.Children.Add(myCircle1);
             canvas.Children.Add(myCircle2);
         }
 
-        public void Animate() 
+        public void Update()
         {
-              double mu = 1+m1/m2;
-              d2Phi1  = (g*(Math.Sin(Phi2)*Math.Cos(Phi1-Phi2)-mu*Math.Sin(Phi1))-(l2*dPhi2*dPhi2+l1*dPhi1*dPhi1*Math.Cos(Phi1-Phi2))*Math.Sin(Phi1-Phi2))/(l1*(mu-Math.Cos(Phi1-Phi2)*Math.Cos(Phi1-Phi2)));
-              d2Phi2  = (mu*g*(Math.Sin(Phi1)*Math.Cos(Phi1-Phi2)-Math.Sin(Phi2))+(mu*l1*dPhi1*dPhi1+l2*dPhi2*dPhi2*Math.Cos(Phi1-Phi2))*Math.Sin(Phi1-Phi2))/(l2*(mu-Math.Cos(Phi1-Phi2)*Math.Cos(Phi1-Phi2)));
-              dPhi1   += d2Phi1*time;
-              dPhi2   += d2Phi2*time;
-              Phi1    += dPhi1*time;
-              Phi2    += dPhi2*time;
+            double myCircle1x = X0+l1*Math.Sin(Phi1);
+            double myCircle1y = Y0+l1*Math.Cos(Phi1);
+            double myCircle2x = X0+l1*Math.Sin(Phi1)+l2*Math.Sin(Phi2);
+            double myCircle2y = Y0+l1*Math.Cos(Phi1)+l2*Math.Cos(Phi2);
+            Canvas.SetLeft(myCircle1, myCircle1x - m1);
+            Canvas.SetTop(myCircle1, myCircle1y - m1);
+            Canvas.SetLeft(myCircle2, myCircle2x - m2);
+            Canvas.SetTop(myCircle2, myCircle2y - m2);
+            myLine1.X2  = myCircle1x;
+            myLine1.Y2  = myCircle1y;
+            myLine2.X1 = myCircle1x;
+            myLine2.Y1 = myCircle1y;
+            myLine2.X2  = myCircle2x;
+            myLine2.Y2  = myCircle2y;
+        }
 
-              double myCircle1x = X0+l1*Math.Sin(Phi1);
-              double myCircle1y = Y0+l1*Math.Cos(Phi1);
-              double myCircle2x = X0+l1*Math.Sin(Phi1)+l2*Math.Sin(Phi2);
-              double myCircle2y = Y0+l1*Math.Cos(Phi1)+l2*Math.Cos(Phi2);
-
-              Canvas.SetLeft(myCircle1, myCircle1x - m1);
-              Canvas.SetTop(myCircle1, myCircle1y - m1);
-              Canvas.SetLeft(myCircle2, myCircle2x - m2);
-              Canvas.SetTop(myCircle2, myCircle2y - m2);
-
-              myLine1.X2  = myCircle1x;
-              myLine1.Y2  = myCircle1y;
-              myLine2.X1 = myCircle1x;
-              myLine2.Y1 = myCircle1y;
-              myLine2.X2  = myCircle2x;
-              myLine2.Y2  = myCircle2y;
+        public void Animate()
+        {
+            double mu = 1+m1/m2;
+            d2Phi1  = (g*(Math.Sin(Phi2)*Math.Cos(Phi1-Phi2)-mu*Math.Sin(Phi1))-(l2*dPhi2*dPhi2+l1*dPhi1*dPhi1*Math.Cos(Phi1-Phi2))*Math.Sin(Phi1-Phi2))/(l1*(mu-Math.Cos(Phi1-Phi2)*Math.Cos(Phi1-Phi2)));
+            d2Phi2  = (mu*g*(Math.Sin(Phi1)*Math.Cos(Phi1-Phi2)-Math.Sin(Phi2))+(mu*l1*dPhi1*dPhi1+l2*dPhi2*dPhi2*Math.Cos(Phi1-Phi2))*Math.Sin(Phi1-Phi2))/(l2*(mu-Math.Cos(Phi1-Phi2)*Math.Cos(Phi1-Phi2)));
+            dPhi1   += d2Phi1*time;
+            dPhi2   += d2Phi2*time;
+            Phi1    += dPhi1*time;
+            Phi2    += dPhi2*time;
+            Update();
         }
     }
 
